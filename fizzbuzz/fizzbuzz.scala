2d11a768b1c47e53297de726874d154e7ef79c23
Nelson Estevão <nelsonmestevao@gmail.com>
2020-04-10 22:17:59 +0100

    Add FizzBuzz solutions

---
+++ fizzbuzz/fizzbuzz.scala
@@ -0,0 +1,15 @@
+object FizzBuzz extends App {
+  var i: Int = 0
+  for(i <- 1 to 100){
+    var result: String = ""
+
+    if (i % 3 == 0) result += "Fizz"
+    if (i % 5 == 0) result += "Buzz"
+
+    if (result.isEmpty()) {
+      println(i)
+    } else {
+      println(result)
+    }
+  }
+}

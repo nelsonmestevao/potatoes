f39e11c17a6664cd7cb3a4027aab1f0f7e6e8e2b
Nelson Estevão <nelsonmestevao@gmail.com>
2020-08-30 13:30:21 +0100

    Reduce code in fizzbuzz problem

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 for (let i = 1; i <= 100; i++) {
-  let result = "";
+    let result = "";
 
-  if (i % 3 === 0) result += "Fizz";
-  if (i % 5 === 0) result += "Buzz";
+    if (i % 3 === 0) result += "Fizz";
+    if (i % 5 === 0) result += "Buzz";
 
-  console.log(result ? result : i);
+    console.log(result || i);
 }
--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -10,4 +10,4 @@ for i in range(1, 101):
     if i % 5 == 0:
         result += "Buzz"
 
-    print(result if result else i)
+    print(result or i)
